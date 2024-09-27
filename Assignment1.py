4bf4342dc3fbfdb5fd549ce4833bdfc0be2da3ef
Edwin Miyatake <122956602+edmiyatake@users.noreply.github.com>
2024-09-27 11:55:48 -0700

    finished 2 more tasks and started another 2

--- Assignment1.py
+++ Assignment1.py
@@ -26,18 +26,53 @@ class LinkedList:
             self.tail.next = newNode
             self.tail = newNode
 
+    def deleteUser(self,user):
+    # function won't make it to very end of the linked list
+    # dummy pointer could work? 
+        curr = self.head
+        if self.head is None:
+            return 0
+        while curr.next:
+            if curr.next.val == user:
+                curr.next = curr.next.next
+                self.size -= 1
+            curr = curr.next
+
+    def payUserToUser(user1,user2,amount):
+        exit
+    
+    # if its odd, return the middle node
+    # if its even, return the average of the two nodes and return the first middle node    
+    def getMedianID(self):
+        if self.head is None:
+            return
+        if self.size % 2 == 1:
+            left,right = self.head,self.head.next.next
+            while right and right.next:
+                left = left.next
+                right = right.next.next
+            print(left.val)
+            return 0
+        else:
+            return -1 
+            
+        
     def printList(self):
         curr = self.head
-        print(self.size)
+        #print(self.size)
         while curr:
             print(curr.val)
             curr = curr.next
+        
 
 list1 = LinkedList()
 list1.addUser("42")
 list1.addUser("32")
 list1.addUser("72")
 list1.addUser("99")
-list1.printList()
+list1.deleteUser("32")
+list1.getMedianID()
+#list1.deleteUser("99")
+#list1.printList()
 
 
