ec91e561fc5ef23a0aad01a4b87e359a02b6d160
Edwin Miyatake <edmiyatake@gmail.com>
2024-09-30 18:25:57 -0700

    Almost done but one test case does not get solved

--- Task2.py
+++ Task2.py
@@ -15,15 +15,39 @@ class LinkedList:
             self.ID.append(counter)
             return counter
     
-    def addUser(self):
-        val = self.assignedUniqueID()
+    def addUser(self,val):
+        # val = self.assignedUniqueID()
         newNode = Node(val)
-        # after this just add to tail
         if self.head is None:
             self.head = newNode
-        
-        
-
+        elif val < self.head.val:
+            # make a pointer before head
+            dummy = Node(0)
+            dummy.next = self.head
+            # perform the swap
+            dummy.next = newNode
+            newNode.next = self.head
+            self.head = newNode
+        else:
+            prev, curr = self.head, self.head.next
+            while curr:
+                if newNode.val > prev.val and newNode.val < curr.val:
+                    print("Searching for the point")
+                    exit
+                prev, curr = curr,curr.next
+            # the two ways the code finishes is if it finds a point or it traverses the whole list
+            # curr == None means it went through the whole list and we can just place the node at the end
+            print(f"Prev is {prev.val}")
+            if curr != None:
+                print("Curr is not None")
+                prev.next = newNode
+                newNode.next = curr
+            # in the event, we have the point where prev represents the spot where the newNode needs to go
+            else:
+                print("Curr is None")
+                prev.next = newNode
+        return 0
+            
     def printList(self):
         curr = self.head
         while curr:
@@ -31,8 +55,13 @@ class LinkedList:
             curr = curr.next
     
 list1 = LinkedList()
-list1.addUser()
-list1.addUser()
+list1.addUser(1)
+list1.addUser(3)
+list1.addUser(4)
+list1.addUser(2)
+list1.addUser(0)
+
+
 list1.printList()
 
 
